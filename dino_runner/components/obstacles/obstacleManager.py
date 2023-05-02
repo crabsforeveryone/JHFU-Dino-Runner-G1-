5ee8eaf80a9e81a82aba80ba898f6ada7d701f0a
crabsforeveryone <jh.f@tuta.io>
2023-05-01 23:24:20 -0400

    tareacactusgrandes
    se añaden los large cactus a los obstaculos

--- dino_runner/components/obstacles/obstacleManager.py
+++ dino_runner/components/obstacles/obstacleManager.py
@@ -1,14 +1,16 @@
 import pygame
+import random
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 class ObstacleManager:
     def __init__(self):
+        self.posibles = [SMALL_CACTUS, LARGE_CACTUS]
         self.obstacles = []
         
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(Cactus(self.posibles[random.randint(0, 1)]))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
